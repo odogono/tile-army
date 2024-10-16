e5071fae91330226be76ac25ce6caf0c81c4b932
Alex Veenendaal <alex@opendoorgonorth.com>
2024-10-16 10:00:32 +0100

    chore: ios build

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '@components/*': './src/components/*',
+            '@helpers/*': './src/helpers/*',
+            '@hooks/*': './src/hooks/*',
+            '@model/*': './src/model/*',
+            '@net/*': './src/net/*',
+            '@/*': './src/*',
+          },
+        },
+      ],
+    ],
   };
 };
